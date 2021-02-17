b577142412b9f2a2faddb3105eaae806249b0d71
PyramidHead01 <hugo4444333@gmail.com>
2021-02-18 00:39:59 +0100

    Implementada peticion REST para recuperar los datos de los pajaros

--- android/CountryBirds/app/src/main/java/com/example/countrybirds/MainActivity.java
+++ android/CountryBirds/app/src/main/java/com/example/countrybirds/MainActivity.java
@@ -2,8 +2,20 @@ package com.example.countrybirds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +28,36 @@ public class MainActivity extends AppCompatActivity {
 
         miTextView = findViewById(R.id.textView2);
 
+        GetRequest();
+
     }
+
+    void GetRequest(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url1 ="http://192.168.0.16/birds/backend/1/pajaro";
+        String url2 ="http://10.0.2.16/birds/backend/1/pajaro";
+        String url = "https://cat-fact.herokuapp.com/facts";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        miTextView.setText(response.toString());
+                        Log.d("GET REQUEST", "PETICION GET CONSEGUIDA");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        miTextView.setText(error.toString());
+                        Log.d("GET REQUEST", error.toString());
+                    }
+                });
+
+        queue.add(jsonArrayRequest);
+    }
+
 }
